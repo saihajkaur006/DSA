46ad1f10fa440325bf0df8e62110cf4dfe497732
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-07-24 12:10:19 +0530

    Time: 1 ms (96.64%) | Memory: 42.4 MB (73.65%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(n<=0){
+            return false;
+        }
+        return (n&(n-1))==0;
+
+
+
+
+
+
+
+
+
+        //     1000
+    //         & 0111
+    //       ------
+    //        0000
+
+    // the power if 2 number always conatin only 1 in it so if we do and with its reverse if we get the 0 then that means its power of 2 otherwise not 
+    }
+}
