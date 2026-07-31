ea21041c45bad5af528ae2f78c963284e4c536d7
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-07-31 23:45:34 +0530

    Time: 1 ms (76.79%) | Memory: 42.5 MB (34.59%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -5,7 +5,7 @@ class Solution {
         while(n!=1 && !seen.contains(n)){
             seen.add(n);
             int sum=0;
-            while(n>0){
+            while(n>0){ 
                 int digit=n%10;
                 sum+=digit*digit;
                 n=n/10;
