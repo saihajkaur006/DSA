7b7d08f78b744a00eee7ec93fbc2cbd07cbcd991
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-08-02 19:43:09 +0530

    Time: 7 ms (79.81%) | Memory: 46.7 MB (65.82%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -2,18 +2,16 @@ class Solution {
     public String shortestPalindrome(String s) {
         StringBuilder rev=new StringBuilder(s);
         rev.reverse();
-
-        String str =s+"$"+rev;
-        int[]LSP=lspcount(str);
-        int ans=s.length()-LSP[LSP.length-1];
+        String str=s+"$"+rev;
+        int[]LSP =lspcount(str);
+        int ans= s.length()-LSP[LSP.length-1];
         String toAdd=rev.substring(0,ans);
-
         return toAdd+s;
     }
     private int[] lspcount(String s){
         int n=s.length();
         int i=1,j=0;
-        int[]LSP = new int[n];
+        int[]LSP=new int[n];
         while(i<n){
             if(s.charAt(i)==s.charAt(j)){
                 LSP[i]=j+1;
@@ -22,6 +20,9 @@ class Solution {
             }else{
                 while(j>0 && s.charAt(i)!=s.charAt(j)){
                     j=LSP[j-1];
+                
+                    
+
                 }
                 if(s.charAt(i)==s.charAt(j)){
                     LSP[i]=j+1;
@@ -29,7 +30,6 @@ class Solution {
                 }
                 i+=1;
             }
-            
         }
         return LSP;
     }
