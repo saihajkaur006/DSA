0af309b59b553eb5ff8dd870dc854e5b06cc894f
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-08-05 14:53:53 +0530

    Time: 15 ms (62.75%) | Memory: 56.1 MB (98.10%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSwaps(String s) {
+        int open=0,close=0;
+        int n=s.length();
+        
+
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='['){
+                open++;
+            }else{
+                if(open>0){
+                    open--;
+                }else{
+                    close++;
+                }
+            }
+        }
+        return (open+1)/2;
+    }
+}
