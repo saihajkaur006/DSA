05034082ad857566e68a83b47ba0ebbd9a3edc07
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-08-05 21:19:00 +0530

    Time: 13 ms (11.20%) | Memory: 47.4 MB (7.12%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -3,12 +3,12 @@ class Solution {
         if(n==1) return "1";
         String prev=countAndSay(n-1);
         int len=prev.length();
-        int count=1;
         String ans="";
+        int count=1;
 
         for(int i=1;i<len;i++){
             if(prev.charAt(i)==prev.charAt(i-1)){
-                count+=1;
+                count++;
             }else{
                 ans+=(char)('0'+count);
                 ans+=prev.charAt(i-1);
@@ -18,5 +18,6 @@ class Solution {
         ans+=(char)('0'+count);
         ans+=prev.charAt(len-1);
         return ans;
+
     }
 }
