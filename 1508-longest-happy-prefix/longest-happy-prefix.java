b55d25616daad730d875fd7930c585b3b8047de9
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-08-05 12:49:32 +0530

    Time: 11 ms (26.29%) | Memory: 47.5 MB (79.46%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.java
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -1,9 +1,9 @@
 class Solution {
     public String longestPrefix(String s) {
-        int[] LPS=lpscount(s);
+        int[]LPS=lpscount(s);
         return s.substring(0,LPS[s.length()-1]);
     }
-    private int[] lpscount(String s){
+    private int[]lpscount(String s){
         int n=s.length();
         int i=1,j=0;
         int[]LPS=new int[n];
@@ -15,16 +15,15 @@ class Solution {
             }else{
                 while(j>0 && s.charAt(i)!=s.charAt(j)){
                     j=LPS[j-1];
-                }
-                if(s.charAt(i)==s.charAt(j)){
+
+                }if(s.charAt(i)==s.charAt(j)){
                     LPS[i]=j+1;
+                    
                     j++;
                 }
                 i+=1;
             }
-
         }
         return LPS;
-
     }
 }
