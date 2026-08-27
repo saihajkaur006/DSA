ebe854d9cac3c44290de190d08fde0ced106ca30
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-08-27 12:14:11 +0530

    Time: 7 ms (81.33%) | Memory: 46.4 MB (34.83%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        List<Integer> temp= new ArrayList<>();
+
+        temp.add(nums[0]);
+        for(int i=0;i<n;i++){
+            if(nums[i]>temp.get(temp.size()-1)){
+                temp.add(nums[i]);
+            }else{
+                int ind=Collections.binarySearch(temp,nums[i]);
+                if(ind<0) ind=-(ind+1);
+                temp.set(ind,nums[i]);
+            }
+        }
+        return temp.size();
+    }
+}
