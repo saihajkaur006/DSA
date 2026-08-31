d05f9c8de4aae2d2efaed6044278be5e573d2da4
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-08-31 23:44:06 +0530

    Time: 3 ms (88.06%) | Memory: 52.4 MB (45.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,54 +1,31 @@
 class Solution {
-    private boolean isValid(int i,int j,int n,int m){
-        if(i<0 || i>=n) return false;
-        if(j<0 || j>=m) return false;
-
-        return true;
-    }
-    private void bfs(int i,int j,boolean[][]vis,char[][]grid){
-        vis[i][j]=true;
-        int n=grid.length;
-        int m=grid[0].length;
-
-        Queue<int[]>q= new LinkedList<>();
-        q.add( new int[]{i,j});
-
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int row = cell[0];
-            int col = cell[1];
-
-            for (int k = 0; k < 4; k++) {
-                int newrow = row + dr[k];
-                int newcol = col + dc[k];
-
-                if (isValid(newrow, newcol, n, m) && grid[newrow][newcol] == '1' && !vis[newrow][newcol]) {
-                    vis[newrow][newcol] = true;
-                    q.add(new int[]{newrow, newcol});
-                }
-            }
-        }
-    }
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int islands = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
 
-        boolean[][]vis=new boolean[n][m];
-        int cnt=0;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j]&& grid[i][j]=='1'){
-                    cnt++;
-                    bfs(i,j,vis,grid);
-
+                if (grid[r][c] == '1') {
+                    islands++;
+                    dfs(grid, r, c);
                 }
-                
             }
         }
-        return cnt;
+        return islands;
+    }
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length ||
+            c < 0 || c >= grid[0].length) {
+            return;
+        }
+        if (grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r + 1, c); 
+        dfs(grid, r - 1, c); 
+        dfs(grid, r, c + 1); 
+        dfs(grid, r, c - 1); 
     }
 }
