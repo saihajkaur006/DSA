6f60a5bbf241ae435bf3765104b3dfa7292971dd
Saihajpreet Kaur <kaursaihaj1515@gmail.com>
2026-09-05 13:49:33 +0530

    Time: 9 ms (61.57%) | Memory: 47.7 MB (47.22%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,41 +1,23 @@
 class Solution {
-     public int lengthOfLongestSubstring(String s) {
-        if (s.length() < 2) {
-            return s.length();
-        }
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        int l=0,r=0,max=0;
 
-        int start = 0;
-        int ll = 1;
-        Map<Character, Integer> found = new HashMap<>();
-        found.put(s.charAt(0), 0);
-        int i;
-        for (i = 1; i < s.length(); i++) {
-            if (found.containsKey(s.charAt(i))) {
-                int newStart = remove(start, i, s.charAt(i), found, s);
+        int hashlen=256;
+        int[] hash = new int[hashlen];
+        Arrays.fill(hash,-1);
 
-                if (i - start > ll) {
-                    ll = i - start;
-                }
-                start = newStart;
-            } else {
-                found.put(s.charAt(i), i);
+        while(r<n){
+            if(hash[s.charAt(r)]!=-1){
+                l=Math.max(l,hash[s.charAt(r)]+1);
             }
+            int len=r-l+1;
+            max=Math.max(len,max);
+
+            hash[s.charAt(r)]=r;
+            r++;
         }
-
-        return Math.max(ll, i - start);
+        return max;
+        
     }
-
-    private int remove(int start, int now, char re, Map<Character, Integer> found, String s) {
-        int j;
-        for (j = start; j < now; j++) {
-            if (s.charAt(j) == re) {
-                found.remove(s.charAt(j));
-                break;
-            }
-            found.remove(s.charAt(j));
-        }
-        found.put(s.charAt(now), now);
-        return j + 1;
-    }
-
 }
